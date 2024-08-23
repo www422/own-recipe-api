c65e09f5b89bb093c424b3838661868ba0ffb5c8
Andr <andronm99@gmail.com>
2024-08-23 14:07:50 +0300

    Added serializer

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -1,17 +1,6 @@
 from rest_framework import serializers
+
 from core.models import Recipe, Tag
-
-
-class RecipeSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Recipe
-        fields = ['id', 'title', 'time_minutes', 'price', 'link']
-        read_only_fields = ['id']
-
-
-class RecipeDetailSerializer(RecipeSerializer):
-    class Meta(RecipeSerializer.Meta):
-        fields = RecipeSerializer.Meta.fields + ['description']
 
 
 class TagSerializer(serializers.ModelSerializer):
@@ -19,3 +8,42 @@ class TagSerializer(serializers.ModelSerializer):
         model = Tag
         fields = ['id', 'name']
         read_only_fields = ['id']
+
+
+class RecipeSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True, required=False)
+
+    class Meta:
+        model = Recipe
+        fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
+        read_only_fields = ['id']
+
+    def _get_or_crate_tags(self, tags, recipe):
+        auth_user = self.context['request'].user
+        for tag in tags:
+            tag_obj, created = Tag.objects.get_or_create(
+                user=auth_user,
+                **tag
+            )
+            recipe.tags.add(tag_obj)
+
+    def create(self, validated_data):
+        tags = validated_data.pop('tags', [])
+        recipe = Recipe.objects.create(**validated_data)
+        self._get_or_crate_tags(tags, recipe)
+        return recipe
+
+    def update(self, instance, validated_data):
+        tags = validated_data.pop('tags', None)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_crate_tags(tags, instance)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
+
+
+class RecipeDetailSerializer(RecipeSerializer):
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
